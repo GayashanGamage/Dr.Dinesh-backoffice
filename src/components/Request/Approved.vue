468bf8d3f6fe585b3f02f0950c7438414b20c87e
gayashan <gayashan.randimagamage@gmail.com>
2024-06-07 18:51:48 +0530

    add basic HTML part to 'approved' and 'Pending' components

--- src/components/Request/Approved.vue
+++ src/components/Request/Approved.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="sc1-level-one-container">
-    <p>this is approved content</p>
+    <div class="sc1-level-two-container">
+      <h4>Client request 1</h4>
+      <hr />
+      <p>Client</p>
+      <p>Chamodi senanayake</p>
+      <p>Package</p>
+      <p>After Advanced level</p>
+      <p>Date</p>
+      <p>2nd of June 2024</p>
+      <p>Time</p>
+      <p>13:00</p>
+      <p>Request</p>
+      <p>4th of June 2024</p>
+      <button>Confirm</button>
+    </div>
   </div>
 </template>
 
--- src/components/Request/Pending.vue
+++ src/components/Request/Pending.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="sc-level-one-container">
-    <p>this is Pending content</p>
+    <div class="sc-level-two-container">
+      <h4>Client request 1</h4>
+      <hr />
+      <p>Client</p>
+      <p>Gayashan Gamage</p>
+      <p>Package</p>
+      <p>After Ordinary level</p>
+      <p>Date</p>
+      <p>10th of May 2024</p>
+      <p>Time</p>
+      <p>20:00</p>
+      <p>Request</p>
+      <p>11th of May 2024</p>
+      <p>Aproved</p>
+      <p>11th of May 2024</p>
+    </div>
   </div>
 </template>
 
